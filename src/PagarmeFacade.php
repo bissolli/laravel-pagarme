14ed66c18d6c384f36792cb3a633b98a2e4d12e5
Gustavo Bissolli <gustavo.bissolli@gmail.com>
2017-05-04 12:38:17 -0300

    Init project

---
+++ src/PagarmeFacade.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bissolli\LaravelPagarme;
+
+
+use Illuminate\Support\Facades\Facade;
+
+class PagarmeFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'pagarme';
+    }
+}
---
+++ src/PagarmeServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Bissolli\LaravelPagarme;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class PagarmeServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+        $this->app->singleton('pagarme', Pagarme::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['pagarme'];
+    }
+}
